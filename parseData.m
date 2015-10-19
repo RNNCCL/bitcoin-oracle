25fbc895a9d810d683c9d19b517e7dd05f83784c
Julio Quiroz <chulini@gmail.com>
2015-10-18 21:26:56 -0300

    parsing data

---
+++ parseData.m
@@ -0,0 +1,30 @@
+% parseData(priCols) convert bitstampUSD.csv to matrix X (consecutive priCols prices and amounts) 
+% and vector y (the price just after those consecutive prices)
+
+function [X,y] = parseData(priCols)
+
+t = cputime;
+data = load('bitstampUSD.csv');
+
+[m n] = size(data);
+
+
+
+X = zeros(1,priCols*2);
+y = zeros(1);
+for i=1:m-priCols,
+	% add prices
+	newExample = data(i:i+priCols-1,2)';
+	% add ammunts
+	newExample = [newExample data(i:i+priCols-1,3)'];
+
+	X = [X;newExample];
+	y = [y;data(i+priCols,2)];
+end
+X(1,:) = [];
+y(1,:) = [];
+printf('Data parsed in in %f seconds\n',cputime-t);
+
+
+end
+
